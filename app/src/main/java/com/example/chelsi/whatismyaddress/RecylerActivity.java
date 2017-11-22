f9010b880870d80e6d7d23caa77a7008488643a1
Chelsi Ampawa <chelsiampawa@gmail.com>
2017-11-21 21:56:36 -0500

    part02 commit

--- app/src/main/java/com/example/chelsi/whatismyaddress/RecylerActivity.java
+++ app/src/main/java/com/example/chelsi/whatismyaddress/RecylerActivity.java
@@ -1,8 +1,37 @@
 package com.example.chelsi.whatismyaddress;
 
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
 /**
  * Created by Chelsi on 11/21/2017.
  */
 
-public class RecylerActivity {
+public class RecylerActivity extends AppCompatActivity{
+    private SharedPreferences sharedPref;
+    private String email;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler);
+
+        ArrayList<String> emails = new ArrayList<>();
+
+
+        Map<String, ?> allEntries = sharedPref.getAll();
+        for (Map.Entry<String, ?> entry : allEntries.entrySet()) {
+            Log.d("map values", entry.getKey() + ": " + entry.getValue().toString());
+            entry.getValue();
+            emails.add(entry.getValue().toString());
+        }
+
+    }
 }
