795fc5f463b69980d5d50252ee63b0f12cbd7c93
Chelsi Ampawa <chelsiampawa@gmail.com>
2017-11-21 22:01:50 -0500

    part02

---
+++ app/src/main/java/com/example/chelsi/whatismyaddress/DisplayActivity.java
@@ -0,0 +1,10 @@
+package com.example.chelsi.whatismyaddress;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Chelsi on 11/21/2017.
+ */
+
+public class DisplayActivity extends AppCompatActivity {
+}
